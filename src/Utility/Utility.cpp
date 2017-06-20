f28d53ff77149a5786f1c89edba9321d9be1dd09
Claudio Cabral <cabral1349@gmail.com>
2017-06-20 17:00:06 +0200

    added tests for Utility.hpp functions, not complete yet

--- src/Utility/Utility.cpp
+++ src/Utility/Utility.cpp
@@ -1,4 +1,8 @@
 #include <gtest/gtest.h>
+#include <glm/vec2.hpp>
+#include <glm/vec3.hpp>
+#include <glm/vec4.hpp>
+#include <glm/ext.hpp>
 #include <Utility/Utility.hpp>
 
 TEST(Utility, ConvertsToGLMVecs) {
@@ -21,3 +25,48 @@ TEST(Utility, ConvertsToGLMVecs) {
 
     ASSERT_EQ(glm_vec4, etudes_vec4);
 }
+
+TEST(Utility, SplitString) {
+    std::string s = "/first/rest/of/string";
+
+    auto first = etudes::util::splitStringFirst(s);
+    ASSERT_EQ(first, "/first");
+
+    auto rest = etudes::util::splitStringRest(s);
+    ASSERT_EQ(rest, "rest/of/string");
+}
+
+TEST(Utility, DegToRad) {
+    ASSERT_EQ(etudes::util::deg2rad(0), 0);
+    ASSERT_EQ(etudes::util::deg2rad(180), glm::pi<float>());
+    ASSERT_EQ(etudes::util::deg2rad(360), 2 * glm::pi<float>());
+    ASSERT_EQ(etudes::util::deg2rad(720), 4 * glm::pi<float>());
+
+    ASSERT_EQ(etudes::util::deg2rad(-180), -1 *glm::pi<float>());
+    ASSERT_EQ(etudes::util::deg2rad(-360), -2 * glm::pi<float>());
+    ASSERT_EQ(etudes::util::deg2rad(-720), -4 * glm::pi<float>());
+}
+
+TEST(Utility, RadToDeg) {
+    ASSERT_EQ(etudes::util::rad2deg(0), 0);
+    ASSERT_EQ(etudes::util::rad2deg(glm::pi<float>()), 180);
+    ASSERT_EQ(etudes::util::rad2deg(glm::pi<float>() * 2), 360);
+    ASSERT_EQ(etudes::util::rad2deg(glm::pi<float>() * 4), 720);
+
+    ASSERT_EQ(etudes::util::rad2deg(glm::pi<float>() * -1), -180);
+    ASSERT_EQ(etudes::util::rad2deg(glm::pi<float>() * -2), -360);
+    ASSERT_EQ(etudes::util::rad2deg(glm::pi<float>() * -4), -720);
+}
+
+// don't really know how to test this...
+//TEST(Utility, ElapsedTime) {
+//
+//}
+
+//TEST(Utility, GLErrors) {
+//
+//}
+//
+//TEST(Utility, registerDefautlShaders) {
+//
+//}
